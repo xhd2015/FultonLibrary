b6efbe841887c3b9347ce23b3483a0e45f718085
SONGZHEN95 <57763950@qq.com>
2016-11-08 15:28:42 +0800

    xhd is my son

---
+++ FultonLibrary/src/fulton/util/android/searcher/Test.java
@@ -0,0 +1,22 @@
+package fulton.util.android.searcher;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.jsoup.nodes.Document;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String word="doyou";
+		String type="zhihu";
+		
+		
+		
+		String url = ContentManager.getUrl(word, type);
+		Document docu = ContentManager.getDocument(url);
+		ContentManager.saveAsFile("C:\\Users\\acer\\Desktop\\zhihu.html", docu.toString());
+	}
+
+}
